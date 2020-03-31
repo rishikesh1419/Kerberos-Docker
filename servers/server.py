3605bd4c8bcbf13dde1fb295a345d60b43251611
Rishikesh <rishikesh1419@gmail.com>
2020-04-01 04:52:07 +0530

    RSA, Timestamps, Error handling to go

--- servers/server.py
+++ servers/server.py
@@ -1,12 +1,14 @@
 import socket
 from datetime import datetime
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
 def check(clientsocket) :
     #
     # RETURN FALSE ???????
     #
-    packet4 = clientsocket.recv(1024).decode()
-    packet4 = packet4.split(",")
+    packet4 = clientsocket.recv(1024)
+    packet4 = packet4.split(b",,,")
     if len(packet4) != 2 :
         # do something
     else :
@@ -15,49 +17,57 @@ def check(clientsocket) :
         #
         # Decrypt ticket3 using private as_tgs_key (RSA)
         #
-        ticket3 = ticket3.split(",")
+        ticket3 = ticket3.split(b",,,")
         if len(ticket3) != 2 :
             # do something
         else :
             uname = ticket3[0]
             key2 = ticket3[1]
+            cipher2 = AES.new(key2, AES.MODE_ECB)
+            timestamp1 = cipher2.decrypt(timestamp)
+            timestamp1a = unpad(timestamp1, 16)
             #
-            # Decrypt timestamp using key2 (DES)
+            # TIMESTAMP FORMAT
             # if timestamp is invalid
+            #
             if False :
                 # do something
-                return False, "", ""
+                return False, b"", b""
             else :
                 #
                 # timestamp1 = timestamp - 1
-                timestamp1 = "temp"
-                #
-                # Encrypt timestamp1 using key2 (DES)
-                #
+                # TIMESTAMP FORMAT
+                timestamp1b = b"temp"
+                timestamp1a = pad(timestamp1b, 16)
+                timestamp1 = cipher2.encrypt(timestamp1a)
                 clientsocket.send(timestamp1.encode())
-                #
-                # Insert (serverID, uname, timestampSelf) to DB
-                #
                 return True, uname, key2
                     
 
 def serve(clientsocket, uname, key2) :
+    #
+    # Start timer
+    #
     while True :
         #
-        # Check timestamp validity from DB
-        #if invalid
+        # Check timer
+        # if invalid
+        #
         if False:
-            # do something, remove from DB, etc...
+            # do something, send msg, etc...
+            clientsocket.close()
         else :
-            packet5 = clientsocket.recv(1024).decode()
+            packetb = clientsocket.recv(1024)
+            cipher2 = AES.new(key2, AES.MODE_ECB)
+            packet5a = cipher2.decrypt(packet5b)
+            packet5 = unpad(packet5a, 16)
             #
-            # Decrypt packet5 using key2 (DES)
             # Process packet5 text
-            result = "temp"
-            # 
-            # Encrypt result using key2 (DES)
             #
-            clientsocket.send(result.encode())
+            result1a = b"temp"
+            result1 = pad(result1a, 16)
+            result = cipher2.encrypt(result1)
+            clientsocket.send(result)
 
 
 
@@ -70,8 +80,8 @@ def main() :
     srv_addr = socket.gethostname()
     srv_port = 4000
     serversocket.bind((srv_addr, srv_port))
+    serversocket.listen(5)
     while True :
-        serversocket.listen(5)
         clientsocket, addr = serversocket.accept()
         print("Client connected!")
         result, uname, key2 = check(clientsocket)
